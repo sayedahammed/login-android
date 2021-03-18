d271d99ceb0fa87b2cd3b48432ae3bbef95fa36b
Sayed Ahammed <sayed.cse17@gmail.com>
2021-03-18 11:40:12 +0600

    empty field check and toast added

--- app/src/main/java/com/example/login/MainActivity.java
+++ app/src/main/java/com/example/login/MainActivity.java
@@ -7,12 +7,14 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn_login;
     EditText et_email, et_password;
     String email, password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +27,20 @@ public class MainActivity extends AppCompatActivity {
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 email = et_email.getText().toString();
                 password = et_password.getText().toString();
+
+                if (email.matches("")) {
+                    Toast.makeText(MainActivity.this, "You did not enter email address", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (password.matches("")) {
+                    Toast.makeText(MainActivity.this, "You did not enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
                 Intent intent = new Intent(MainActivity.this, DetailsActivity.class);
                 intent.putExtra("email", email);
                 intent.putExtra("password", password);
